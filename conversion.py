be3c558523a850c5551e9b1c4910e23719d2aa8d
kensim48 <kensim28@hotmail.com>
2021-03-18 00:00:35 +0800

    PEP8 formatting

--- conversion.py
+++ conversion.py
@@ -22,9 +22,9 @@ print("Done converting")
 
 txt_list = []
 for jpg_name in jpg_list:
-	txt_name = jpg_name[:-4] + ".txt"
-	txt_list.append(txt_name)
-	os.system("jp2a --width=200 " + jpg_name + " --output=" + txt_name)
+    txt_name = jpg_name[:-4] + ".txt"
+    txt_list.append(txt_name)
+    os.system("jp2a --width=200 " + jpg_name + " --output=" + txt_name)
 print("Done creating text files")
 os.system("rm " + directory + "/*.jpg")
 print("Done deleting images")
@@ -36,11 +36,11 @@ result_anim = open("cube.html", "w+")
 result_anim.write(web_pre.read())
 
 for txt_name in txt_list:
-	single_text = open(txt_name, "r")
-	result_anim.write("\n<pre style=\"display: none;\">")
-	result_anim.write(single_text.read())
-	result_anim.write("</pre>\n")
-	single_text.close()
+    single_text = open(txt_name, "r")
+    result_anim.write("\n<pre style=\"display: none;\">")
+    result_anim.write(single_text.read())
+    result_anim.write("</pre>\n")
+    single_text.close()
 
 result_anim.write(web_post.read())
 
