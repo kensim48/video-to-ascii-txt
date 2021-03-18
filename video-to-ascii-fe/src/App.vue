31766c02bd3b20b4bdc97234a6986c75ea579d52
Kenneth Sim <kensim28@hotmail.com>
2021-03-18 15:18:44 +0800

    Updated vue with animated title

--- video-to-ascii-fe/src/App.vue
+++ video-to-ascii-fe/src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Title class="animatedfTitle" msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Title from "./components/Title.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Title,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align: center; */
+  background-color: black;
+  color: white;
+  /* margin-top: 60px; */
+}
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+  .animatedfTitle {
+    display: inline-block;
+    font-size: 0.8vw;
+    /* width: 50%; */
+  }
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+  .animatedfTitle {
+    display: inline-block;
+    font-size: 0.4vw;
+    /* width: 50%; */
+  }
 }
 </style>
